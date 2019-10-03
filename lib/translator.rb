1afbd76783d5f7df44349e3b5f678833595829c5
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-10-03 00:57:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,6 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
 def get_english_meaning(file_path, emoticon)
   #binding.pry
   load_library(file_path)
+  load_library(file_path)[:get_meaning][emoticon]
   
 end  
 
