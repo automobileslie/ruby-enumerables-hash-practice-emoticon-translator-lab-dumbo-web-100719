e9b813a62ec6ff46d45db5e9a6fe49c2c1a1e86e
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-10-03 01:00:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,10 +26,16 @@ def get_japanese_emoticon(file_path, emoticon)
 
 def get_english_meaning(file_path, emoticon)
   #binding.pry
-  load_library(file_path)
-  load_library(file_path)[:get_meaning][emoticon]
+ load_library(file_path)
+ the_meaning = load_library(file_path)[:get_meaning][emoticon]
+ 
+  if the_meaning
+      the_meaning
+  else
+      return "Sorry, that emoticon was not found"
+  end
+ end
   
-end  
 
 # {"angel"=>["O:)", "☜(⌒▽⌒)☞"],
 # "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
